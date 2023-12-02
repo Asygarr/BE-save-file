fa310c5e0c9aa9e6aefae020cdfbb564ab1588c1
Asygarr <105841104721@student.unismuh.ac.id>
2023-12-02 12:19:54 +0800

    configurasi prisma session store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
-import { PrismaSessionModule } from './middleware/prisma-session-store/prisma-session.module';
 
 @Module({
   providers: [],
-  imports: [UsersModule, PostsModule, PrismaSessionModule],
+  imports: [UsersModule, PostsModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as session from 'express-session';
+import { PrismaClient } from '@prisma/client';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prisma = new PrismaClient();
 
   dotenv.config();
+  
+  app.use(
+    session({
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+      },
+      secret: process.env.SESSION_SECRET,  // Sesuaikan dengan secret Anda
+      resave: true,
+      saveUninitialized: true,
+      store: new PrismaSessionStore(prisma, {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    }),
+  );
 
   await app.listen(3000);
 }
