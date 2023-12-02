c623e1707f96b41f625c9cd11e5b1d4cbb352dde
Asygarr <105841104721@student.unismuh.ac.id>
2023-12-02 15:53:37 +0800

    menghubungkan middleware dengan controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { VerifyUserMiddleware } from './middleware/verify-user.middleware';
+import { UsersController } from './users/users.controller';
+import { PostsController } from './posts/posts.controller';
 
 @Module({
-  providers: [],
   imports: [UsersModule, PostsModule, AuthModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(VerifyUserMiddleware)
+      .forRoutes(UsersController, PostsController);
+  }
+}
