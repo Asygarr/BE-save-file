37935572b07460c63ce97c8063fdfc71f3b36736
Asygarr <105841104721@student.unismuh.ac.id>
2023-12-02 12:18:16 +0800

    selesai membuat crud user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
-/* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,27 +8,83 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: any) {
+    try {
+      const createUser = await this.usersService.create(createUserDto);
+
+      return res.status(HttpStatus.CREATED).json({
+        message: 'Berhasil membuat user',
+        data: createUser,
+      })
+    } catch (error) {
+      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: error.message,
+      })
+    }
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(@Res() res: any) {
+    try {
+      const users = await this.usersService.findAll();
+
+      return res.status(HttpStatus.OK).json({
+        message: 'User yang terdaftar',
+        data: users,
+      })
+    } catch (error) {
+      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: error.message,
+      })
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string, @Res() res: any) {
+    try {
+      const user = await this.usersService.findOne(id);
+
+      return res.status(HttpStatus.OK).json({
+        message: 'User yang terdaftar',
+        data: user,
+      })
+    } catch (error) {
+      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: error.message,
+      })
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @Res() res: any) {
+    try {
+      const updateUser = await this.usersService.update(id, updateUserDto);
+
+      return res.status(HttpStatus.OK).json({
+        message: 'Berhasil mengubah user',
+        data_lama: updateUser.dataLama,
+        data_baru: updateUser.dataBaru,
+      })
+    } catch (error) {
+      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: error.message,
+      })
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string, @Res() res: any) {
+    try {
+      const deleteUser = await this.usersService.remove(id);
+
+      return res.status(HttpStatus.OK).json({
+        message: 'Berhasil menghapus user',
+        data: deleteUser,
+      })
+    } catch (error) {
+      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: error.message,
+      })
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,161 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import * as argon from 'argon2';
+import { parse } from 'path';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, name, password, confPassword } = createUserDto;
+
+    if (!email || !name || !password || !confPassword) {
+      throw new HttpException('Data tidak lengkap', HttpStatus.BAD_REQUEST);
+    }
+
+    const cekUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+
+    if (cekUser) {
+      throw new HttpException('Email sudah terdaftar', HttpStatus.BAD_REQUEST);
+    }
+
+    if (password !== confPassword) {
+      throw new HttpException('Password tidak sama', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashedPassword = await argon.hash(password);
+    return this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword
+      }
+    })
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const cekUSer = await this.prisma.user.findUnique({
+      where: {
+        id: id
+      }
+    })
+
+    if (!cekUSer) {
+      throw new HttpException('User tidak ditemukan', HttpStatus.NOT_FOUND);
+    }
+
+    return this.prisma.user.findUnique({
+      where: {
+        id: id
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const { email, name, password, confPassword } = updateUserDto;
+
+    if (!email || !name || !password || !confPassword) {
+      throw new HttpException('Data tidak lengkap', HttpStatus.BAD_REQUEST);
+    }
+
+    const cekUser = await this.prisma.user.findUnique({
+      where: {
+        id: id
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
+
+    const dataLama = cekUser;
+
+    if (!cekUser) {
+      throw new HttpException('User tidak ditemukan', HttpStatus.NOT_FOUND);
+    }
+
+    if (password !== confPassword) {
+      throw new HttpException('Password tidak sama', HttpStatus.BAD_REQUEST);
+    }
+
+    const hashedPassword = await argon.hash(password);
+
+    const dataBaru = await this.prisma.user.update({
+      where: {
+        id: id
+      },
+      data: {
+        email,
+        name,
+        password: hashedPassword
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
+
+    return {
+      dataLama,
+      dataBaru
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const cekUser = await this.prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!cekUser) {
+      throw new HttpException('User tidak ditemukan', HttpStatus.NOT_FOUND);
+    }
+
+    return this.prisma.user.delete({
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
   }
 }
