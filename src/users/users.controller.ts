27acd2ec07aa35b945852f1892778c60fa13189b
Asygarr <105841104721@student.unismuh.ac.id>
2023-12-04 23:25:20 +0800

    memakaikan respon handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,61 +8,48 @@ import {
   Delete,
   Res,
   HttpStatus,
+  HttpException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ResponseHandler } from 'src/utils/response-handler';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly responseHandle: ResponseHandler,
+  ) {}
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto, @Res() res: any) {
-    try {
-      const createUser = await this.usersService.create(createUserDto);
-
-      return res.status(HttpStatus.CREATED).json({
-        message: 'Berhasil membuat user',
-        data: createUser,
-      });
-    } catch (error) {
-      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message,
-      });
-    }
+    return this.responseHandle.handle(
+      res,
+      this.usersService.create(createUserDto),
+      HttpStatus.CREATED,
+      'Berhasil membuat user'
+    )
   }
 
   @Get()
   async findAll(@Res() res: any) {
-    try {
-      const users = await this.usersService.findAll();
-
-      return res.status(HttpStatus.OK).json({
-        message: 'User yang terdaftar',
-        data: users,
-      });
-    } catch (error) {
-      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message,
-      });
-    }
+    return this.responseHandle.handle(
+      res,
+      this.usersService.findAll(),
+      HttpStatus.OK,
+      'User yang terdaftar',
+    )
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string, @Res() res: any) {
-    try {
-      const user = await this.usersService.findOne(id);
-
-      return res.status(HttpStatus.OK).json({
-        message: 'User yang terdaftar',
-        data: user,
-      });
-    } catch (error) {
-      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message,
-      });
-    }
+    return this.responseHandle.handle(
+      res,
+      this.usersService.findOne(id),
+      HttpStatus.OK,
+      'User yang dicari',
+    )
   }
 
   @Patch(':id')
@@ -71,34 +58,21 @@ export class UsersController {
     @Body() updateUserDto: UpdateUserDto,
     @Res() res: any,
   ) {
-    try {
-      const updateUser = await this.usersService.update(id, updateUserDto);
-
-      return res.status(HttpStatus.OK).json({
-        message: 'Berhasil mengubah user',
-        data_lama: updateUser.dataLama,
-        data_baru: updateUser.dataBaru,
-      });
-    } catch (error) {
-      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message,
-      });
-    }
+    return this.responseHandle.handle(
+      res,
+      this.usersService.update(id, updateUserDto),
+      HttpStatus.OK,
+      'Berhasil mengubah user',
+    )
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string, @Res() res: any) {
-    try {
-      const deleteUser = await this.usersService.remove(id);
-
-      return res.status(HttpStatus.OK).json({
-        message: 'Berhasil menghapus user',
-        data: deleteUser,
-      });
-    } catch (error) {
-      return res.status(error.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: error.message,
-      });
-    }
+    return this.responseHandle.handle(
+      res,
+      this.usersService.remove(id),
+      HttpStatus.OK,
+      'Berhasil menghapus user',
+    )
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaClient } from '@prisma/client';
+import { ResponseHandler } from 'src/utils/response-handler';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaClient],
+  providers: [UsersService, PrismaClient, ResponseHandler],
   exports: [UsersService],
 })
 export class UsersModule {}
