2abd5acfdf8ef370bd7e52dc535a9238bce0c922
Asygarr <105841104721@student.unismuh.ac.id>
2023-12-07 23:53:23 +0800

    mengimport depedencies

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { ResponseHandler } from 'src/utils/response-handler';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, ResponseHandler, PrismaClient],
 })
 export class PostsModule {}
